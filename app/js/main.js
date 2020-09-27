2604584e03e278b7ff13ee52e34b0be460455805
yegorPomaskin <yegor.pomaskin@gmail.com>
2020-09-27 19:19:57 +0300

    added features-slider

--- app/js/main.js
+++ app/js/main.js
@@ -7,7 +7,20 @@ $(function(){
     nextArrow:"<img class='a-right control-c next slick-next' src='../images/icons/arrow-right.png'>"
   });
 
+  $('.features__slider-inner').slick({
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    prevArrow:"<img class='a-left control-c prev slick-prev' src='../images/icons/arrow-left.png'>",
+    nextArrow:"<img class='a-right control-c next slick-next' src='../images/icons/arrow-right.png'>"
+  })
+
   $(".rate-star").rateYo({
+    rating: 5,
+    starWidth: "12px",
+    readOnly: true
+  });
+
+  $(".products__item-rate").rateYo({
     rating: 5,
     starWidth: "12px",
     readOnly: true
