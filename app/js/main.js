d0f89c2038416d9c2ed2c1732dab03133cf23a28
yegorPomaskin <yegor.pomaskin@gmail.com>
2020-10-18 12:57:17 +0300

    added login

--- app/js/main.js
+++ app/js/main.js
@@ -52,6 +52,15 @@ $(function(){
     $('.menu__list').slideToggle('active');
   });
 
+  $('.product-one__tabs .tab').on('click', function(event) {
+    var id = $(this).attr('data-id');
+      $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+      $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+      $(this).addClass('active');
+      $('#'+id).addClass('active-tab').fadeIn();
+      return false;
+    });
+
   var mixer = mixitup('.products__inner-box');
 
 
